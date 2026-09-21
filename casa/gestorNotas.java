42741f3043f1886b185dc97a0c1158af02f955ef
srXendo <73126779+srXendo@users.noreply.github.com>
2026-09-21 16:17:37 +0200

    add: formato

--- casa/gestorNotas.java
+++ casa/gestorNotas.java
@@ -11,10 +11,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class gestorNotas{
+public class gestorNotas {
+
     public static final String DELIMITADOR_COLUMNA = ";";
     public static final String DELIMITADOR_NOTAS = "---";
-    static public void main(String[] args) throws IOException{
+
+    static public void main(String[] args) throws IOException {
 
         Scanner teclado = new Scanner(System.in);
 
@@ -26,16 +28,16 @@ public class gestorNotas{
 
         Alumnos alumnos = cargaDatos(fichero);
 
-        do{
+        do {
             option = muestraMenu(teclado);
-            
-            switch(option){
+
+            switch (option) {
                 //salir
-                case -1: 
+                case -1:
                     break;
 
                 //añadir alumno    
-                case 1: 
+                case 1:
                     alumnos.addAlumno(teclado);
                     break;
 
@@ -53,72 +55,75 @@ public class gestorNotas{
                 case 4:
                     guardaDatosAlumnos(fichero, alumnos);
                     break;
-                    
+
             }
-        }while(option > 0);
+        } while (option > 0);
         teclado.close();
     }
+
     //Recupera datos del fichero
-    static Alumnos cargaDatos(File fichero) throws FileNotFoundException, IOException{
-        
+    static Alumnos cargaDatos(File fichero) throws FileNotFoundException, IOException {
+
         BufferedReader lector = new BufferedReader(new FileReader(fichero));
         String fila = lector.readLine();
         Alumnos alumnos = new Alumnos();
-        while(fila != null){
+        while (fila != null) {
             String[] columnas = fila.split(DELIMITADOR_COLUMNA);
             String nombreAlumno = columnas[0];
             String[] notasAlumno = columnas[1].split(DELIMITADOR_NOTAS);
             alumnos.creaAlumnoSiNoExiste(nombreAlumno);
-            for(String nota: notasAlumno ){
-                
+            for (String nota : notasAlumno) {
+
                 double notaReal = Double.parseDouble(nota);
                 alumnos.mapAlumnos
-                    .get(nombreAlumno)
-                    .add(notaReal);
-                
+                        .get(nombreAlumno)
+                        .add(notaReal);
+
             }
-            
+
         }
         lector.close();
         return alumnos;
     }
+
     //Guarda datos en el fichero
-    static void guardaDatosAlumnos(File fichero, Alumnos alumnos) throws FileNotFoundException, IOException{
+    static void guardaDatosAlumnos(File fichero, Alumnos alumnos) throws FileNotFoundException, IOException {
         BufferedWriter escritor = new BufferedWriter(new FileWriter(fichero));
-        
-        for(String nombreAlumno: alumnos.mapAlumnos.keySet()){
+
+        for (String nombreAlumno : alumnos.mapAlumnos.keySet()) {
             System.out.println("nombre alumno: " + nombreAlumno);
             ArrayList<Double> notasAlumno = alumnos.mapAlumnos.get(nombreAlumno);
             String linea = nombreAlumno + DELIMITADOR_COLUMNA;
-            for(int i = 0; i < notasAlumno.size(); i++){
-                if(i != 0){
-                        linea = linea + DELIMITADOR_NOTAS;
+            for (int i = 0; i < notasAlumno.size(); i++) {
+                if (i != 0) {
+                    linea = linea + DELIMITADOR_NOTAS;
                 }
                 linea = linea + notasAlumno.get(i);
             }
             escritor.write(linea);
-            
+
         }
         escritor.close();
 
     }
+
     //Pide por pantalla el nombre del archivo y devuelve lo escrito.
-    static public String dameNomombreFichero(Scanner teclado){
+    static public String dameNomombreFichero(Scanner teclado) {
         System.out.print("Escribe nombre del fichero: ");
         String nombre = teclado.nextLine();
         return nombre;
     }
 
-    static public File dameFichero (String nombreFichero) throws IOException{
+    static public File dameFichero(String nombreFichero) throws IOException {
         //instanciamos fichero y comprobamos si existe
         File fichero = new File(nombreFichero);
-        if(!fichero.exists()){
+        if (!fichero.exists()) {
             fichero.createNewFile();
         }
         return fichero;
     }
 
-    static public int muestraMenu(Scanner s){
+    static public int muestraMenu(Scanner s) {
         System.out.println("-------Menu-------");
         System.out.println("-1. salir");
         System.out.println(" 1. Añadir alumno");
@@ -132,61 +137,65 @@ public class gestorNotas{
     }
 }
 
-class Alumnos{
-    Map<String, ArrayList<Double>> mapAlumnos = new HashMap<>(); 
-     
-    public void addAlumno(Scanner s){
+class Alumnos {
+
+    Map<String, ArrayList<Double>> mapAlumnos = new HashMap<>();
+
+    public void addAlumno(Scanner s) {
         String nombre = this.pideNombreAlumno(s);
         creaAlumnoSiNoExiste(nombre);
     }
 
-    public void addNota(Scanner s){
+    public void addNota(Scanner s) {
         String nombre = this.pideNombreAlumno(s);
         creaAlumnoSiNoExiste(nombre);
 
         Double nota = this.pideNotaAlumno(s);
 
-        this.mapAlumnos.get(nombre).add(nota); 
+        this.mapAlumnos.get(nombre).add(nota);
 
         System.out.printf("Nota añadida correctamente: %s ---- %f%n", nombre, nota);
         System.out.println(this.mapAlumnos);
     }
-    public void creaAlumnoSiNoExiste(String nombre){
+
+    public void creaAlumnoSiNoExiste(String nombre) {
         //comprueba si existe el alumno
 
-        if(this.mapAlumnos.get(nombre) == null){
+        if (this.mapAlumnos.get(nombre) == null) {
             this.mapAlumnos.put(nombre, new ArrayList<Double>());
         }
     }
-    public String pideNombreAlumno(Scanner s){
+
+    public String pideNombreAlumno(Scanner s) {
         System.out.print("Escribe el nombre del alumno  : ");
         String nombre = s.nextLine();
         return nombre;
     }
 
-    public double pideNotaAlumno(Scanner s){
+    public double pideNotaAlumno(Scanner s) {
         System.out.print("Escribe la nota del alumno    : ");
         double nota = s.nextDouble();
         s.nextLine();
         return nota;
     }
 
-    public void muestraInforme(Scanner s){
+    public void muestraInforme(Scanner s) {
         String nombreAlumno = this.pideNombreAlumno(s);
 
         creaAlumnoSiNoExiste(nombreAlumno);
-        
-        ArrayList<Double> arrayListNotas = this.mapAlumnos.get(nombreAlumno); 
-        System.out.println(arrayListNotas);
+
+        ArrayList<Double> arrayListNotas = this.mapAlumnos.get(nombreAlumno);
+
         double mediaNotas = 0.0;
         int numAprovados = 0;
         int numSuspensos = 0;
         double minNota = 0;
         double maxNota = 0;
-        double sumaNota = 0; 
-        for(double nota: arrayListNotas){
+        double sumaNota = 0;
         
-            if(sumaNota == 0){
+        for (double nota : arrayListNotas) {
+
+            if (sumaNota == 0) {
                 minNota = nota;
                 maxNota = nota;
             }
@@ -194,16 +203,16 @@ class Alumnos{
             sumaNota += nota;
 
             //comprueba la nota minima y maxima
-            if(minNota > nota){
+            if (minNota > nota) {
                 minNota = nota;
             }
-            if(maxNota < nota){
+            if (maxNota < nota) {
                 maxNota = nota;
             }
 
-            if(nota < 5){
+            if (nota < 5) {
                 numSuspensos += 1;
-            }else{
+            } else {
                 numAprovados += 1;
             }
 
@@ -213,13 +222,13 @@ class Alumnos{
         System.out.println("---------INFORME DE ALUMNO---------");
         //nombreAlumno
         System.out.printf("Nombre           : %s%n", nombreAlumno);
-        
+
         //numeroNotas
         System.out.printf("numero de notas  : %d%n", arrayListNotas.size());
 
         //mediaNotas
         System.out.printf("media de notas   : %f%n", mediaNotas);
-        
+
         //numeroAprovadas
         System.out.printf("Aprovados        : %d%n", numAprovados);
 
@@ -233,7 +242,7 @@ class Alumnos{
         System.out.printf("Nota minima      : %f%n", minNota);
 
         System.out.println("----------FIN DEL INFORME----------");
-        
+
     }
 
 }
